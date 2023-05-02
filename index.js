15d86150d13ef3fc0a1ae22d828cadc3a8aaee91
helloashik17 <asiknoman150@gmail.com>
2023-05-03 05:10:57 +0600

    server site created

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+// const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// const chefs = require('./data/chefs.json');
+
+// app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Kitchen is running');
+});
+
+// app.get('/chefs', (req, res) => {
+//     res.send(chefs);
+// });
+
+// app.get('/chefs/:id', (req, res) => {
+//     const id = req.params.id;
+//     const selectedChef = chefs.find(n => n._id === id);
+//     res.send(selectedChef);
+// });
+
+app.listen(port, () => {
+    console.log(`Kitchen API is running on port: ${port}`);
+});
