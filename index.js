d87c8a376815371b5f866f62413c7f2eb85f2e2c
helloashik17 <asiknoman150@gmail.com>
2023-05-03 06:15:01 +0600

    chefs data fetched on localhost

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 // const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-// const chefs = require('./data/chefs.json');
+const chefs = require('./data/chefs.json');
 
 // app.use(cors());
 
@@ -11,9 +11,9 @@ app.get('/', (req, res) => {
     res.send('Kitchen is running');
 });
 
-// app.get('/chefs', (req, res) => {
-//     res.send(chefs);
-// });
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+});
 
 // app.get('/chefs/:id', (req, res) => {
 //     const id = req.params.id;
