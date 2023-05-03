d890c62f6205f016d4a7b8f2b26de643b2b15bf1
helloashik17 <asiknoman150@gmail.com>
2023-05-03 06:19:46 +0600

    chefs single data fetched on localhost

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
 
-// app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Kitchen is running');
@@ -15,11 +15,11 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 });
 
-// app.get('/chefs/:id', (req, res) => {
-//     const id = req.params.id;
-//     const selectedChef = chefs.find(n => n._id === id);
-//     res.send(selectedChef);
-// });
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.find(n => n._id === id);
+    res.send(selectedChef);
+});
 
 app.listen(port, () => {
     console.log(`Kitchen API is running on port: ${port}`);
